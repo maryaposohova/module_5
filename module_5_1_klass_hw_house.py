c4bb8f014811abc52ad71ce5b8d8782b0b12a4ad
maryaposohova <maryaposohova1202@gmail.com>
2024-09-10 16:42:34 +0500

    Add files via upload

---
+++ module_5_1_klass_hw_house.py
@@ -0,0 +1,23 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor+1):
+            new_floor = i
+            print(f"{new_floor}")
+            if new_floor > self.number_of_floors or new_floor < 1:
+                print('Такого этажа не существует')
+                return
+
+
+
+h1 = House('ЖК Горский', 18)
+h1.go_to(5)
+# h2 = House('Домик в деревне', 2)
+# h2.go_to(10)
+
+
