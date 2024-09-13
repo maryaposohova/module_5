0c8aaca2bb555c711724d20e73d919d8c5ef593e
maryaposohova <maryaposohova1202@gmail.com>
2024-09-13 17:53:11 +0500

    Add files via upload

--- Klass_module_5.py
+++ Klass_module_5.py
@@ -10,14 +10,39 @@ class Human:
         self.age += 1
         print(f"У меня день рождения, мне теперь {self.age}")
 
+    def __len__(self):
+        return self.age
+
+    def __lt__(self, other):
+        return self.age < other.age
+
+    def __gt__(self, other):
+        return self.age > other.age
+
+
+    def __eq__(self, other):
+        return self.age == other.age
+
+    def __eq__(self, other):
+        return self.name == other.age and self.age == other.age
+
+    def __del__(self):
+        print(f"{self.name} ушел")
+
+
+
 
 den = Human('Денис', 22)
 max = Human('Максим', 22)
-jur = Human("Юргис", 18)
-max.birthday()
-den.birthday()
-jur.birthday()
-max.birthday()
-den.birthday()
-jur.birthday()
+max.name = 'Денис'
+print(max == den)
+# jur = Human("Юргис", 18)
+# print('Дену 22, Максу еще 22',den == max)
 
+max.birthday()      # False, разные имена
+# jur.birthday()
+# print('Дену 22, Максу уже 23',den < max)
+# print('Дену 22, Максу уже 23',den > max)
+# print('Дену 22, Максу уже 23', den == max)
+#
+# print(max == den)  # False, разные имена и возраст
