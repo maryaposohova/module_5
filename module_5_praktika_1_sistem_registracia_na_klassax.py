220751d4c3cab509f4a6494453308e8a5ef4ce8e
maryaposohova <maryaposohova1202@gmail.com>
2024-09-21 11:16:40 +0500

    Add files via upload

--- module_5_praktika_1_sistem_registracia_na_klassax.py
+++ module_5_praktika_1_sistem_registracia_na_klassax.py
@@ -1,19 +1,34 @@
+class Database: # база данных
+    def __init__(self):
+        self.data = {}  # селф дата в виде пустого словаря
+    def add_user(self, username, password):  # будет добавлять юзера и  принимать юзернейм и пассворд
+        self.data[username] = password      # будет принимать значение по ключу и сохранять в пароль значение пароля
+
+
+
 class User:   # Создаем пользователя
     """
-    Класс пользователя, содержащий атрибуты
+    Класс пользователя, содержащий атрибуты: логин, пароль
     """
     def __init__(self, username, password, password_confirm):
         self.username = username
         if password == password_confirm: # если пароль будет совпадать с пасворд конфирм, только тогда будем выдавать пароль
             self.password = password
+        else:
+            self.password =
 
 
 
+# сщздаем пользователя
+
+if __name__ == "__main__":  # входная точка в нашу программу,
+    database = Database()   # где создадим базу данных
+    user = User(input('Введите логин: '), input('Введите пароль: '), input('Повторите пароль: '))   # запрашиваем пользовательский ввод
+    database.add_user(user.username, user.password) # добавляем пользователя в базу данных, т.е. берем обьект (базу данных) и вызовем метод эдд_юзер, куда мы из юзера передадим юзернейм и пароль
 
 
 
-
-
+print(database.data)
 
 
 
