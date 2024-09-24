e550a190c91179eeb24e72149ba318bc416852bf
maryaposohova <maryaposohova1202@gmail.com>
2024-09-24 14:28:23 +0500

    Add files via upload

---
+++ module_5_4_hw_construction_history_dopolnenie.py
@@ -0,0 +1,37 @@
+
+class House:
+    houses_history = []
+
+    def __new__(cls, name, number_of_floors):
+         a = super().__new__(cls)
+         House.houses_history.append(name)
+         return a
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+
+
+
+    def __del__(self):
+        print(f'"{self.name} снесён, но он останется в истории"')
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
+
+
